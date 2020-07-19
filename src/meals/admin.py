05d2d9bce490942f0da366ee342868020f1f73f7
Daniel Reyes Veas <danielreyesveas@gmail.com>
2020-07-19 19:40:56 -0400

    Cafe Cuqui

--- src/meals/admin.py
+++ src/meals/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Meal, Category
 
-class MealAdmin(SummernoteModelAdmin):
-    summernote_fields = ['description']
+class MealAdmin(admin.ModelAdmin):
     list_display = ['name', 'preparation_time', 'people', 'price']
     list_filter = ['category', 'people']
     search_fields = ['name', 'description']
