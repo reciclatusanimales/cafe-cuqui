f8b7e43c638eb0f28ab02ebb5950d39f7ef5003b
Daniel Reyes Veas <danielreyesveas@gmail.com>
2021-03-08 21:37:20 +0100

    API_KEY

--- contact/constants.py
+++ contact/constants.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-
 contact_template_slug =  'cafe_cuqui_contact'
 newsletter_template_slug =  'cafe_cuqui_newsletter'
 email_sender_url = settings.EMAIL_SENDER_URL
+email_sender_api_key = settings.EMAIL_SENDER_API_KEY
--- contact/views.py
+++ contact/views.py
@@ -5,7 +5,7 @@ from .utils import api_response
 import requests
 import time
 import json
-from .constants import contact_template_slug, newsletter_template_slug, email_sender_url
+from .constants import contact_template_slug, newsletter_template_slug, email_sender_url, email_sender_api_key
 
 def contact(request):
     form = ContactForm
@@ -36,13 +36,16 @@ def send_email(request):
     attempt_num = 0
     while attempt_num < 1:       
         body = {'name': name, 'from': email, 'subject': subject, 'content': content, 'template_slug': contact_template_slug, 'type': 'contact'}
-        response = requests.post(email_sender_url, data = json.dumps(body))
+        headers = {'api-key': email_sender_api_key}
+        response = requests.post(email_sender_url, data = json.dumps(body), headers=headers)
         if response.status_code == 200:
             data = response.json()
             return JsonResponse(data)
         else:
             attempt_num += 1
             time.sleep(5) 
+    
+    json_response["msg"] = "Hubo un problema con la solicitud."
 
     return JsonResponse(json_response)
 
@@ -60,12 +63,15 @@ def subscribe_newsletter(request):
     attempt_num = 0
     while attempt_num < 1:       
         body = {'from': email, 'template_slug': newsletter_template_slug, 'type': 'newsletter'}
-        response = requests.post(email_sender_url, data = json.dumps(body))
+        headers = {'api-key': email_sender_api_key}
+        response = requests.post(email_sender_url, data = json.dumps(body), headers=headers)
         if response.status_code == 200:
             data = response.json()
             return JsonResponse(data)
         else:
             attempt_num += 1
             time.sleep(5) 
+
+    json_response["msg"] = "Hubo un problema con la solicitud."
 
     return JsonResponse(json_response)
