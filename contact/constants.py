fb4a3f5cbe4e076213e8e15abde54df23283d3f0
Daniel Reyes Veas <danielreyesveas@gmail.com>
2021-03-08 19:31:37 +0100

    from django.conf import settings

--- contact/constants.py
+++ contact/constants.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+
+
 contact_template_slug =  'cafe_cuqui_contact'
 newsletter_template_slug =  'cafe_cuqui_newsletter'
-email_sender_url = 'http://localhost:8080/add-email'
+email_sender_url = settings.EMAIL_SENDER_URL
