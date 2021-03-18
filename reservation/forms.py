b682474a01e75ca88030924c514850ea48a3a975
Daniel Reyes Veas <danielreyesveas@gmail.com>
2021-03-18 19:21:20 +0100

    CSS

--- reservation/forms.py
+++ reservation/forms.py
@@ -3,6 +3,13 @@ from .models import Reservation
 
 
 class ReserveTableForm(forms.ModelForm):
+    name = forms.CharField(required=True, label="Nombre")
+    email = forms.EmailField(required=True, label="Email")
+    phone = forms.IntegerField(required=True, label="Teléfono")
+    number_of_persons = forms.IntegerField(required=True, label="¿Para cuántos?")
+    date = forms.DateField(required=True, label="Día")
+    time = forms.TimeField(required=True, label="Hora")
+
     class Meta:
         model = Reservation
-        fields = '__all__'
+        fields = ['name', 'email', 'phone', 'number_of_persons', 'date', 'time']
